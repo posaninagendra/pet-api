0c75a0f12b3e387b0e049f1b7088ef99e015d688
Kevin Petrychyn <petrychyn@gmail.com>
2017-02-22 17:13:44 -0600

    first attempt at get by id

--- index.js
+++ index.js
@@ -4,12 +4,10 @@ var pg = require('pg');
 pg.defaults.ssl = true;
 
 app.get('/pets', function(req, res) {
-  console.log('URL = ' + process.env.DATABASE_URL);
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
     if ( err ) {
       throw err;
     }
-    console.log('connected to db');
     client.query('SELECT * FROM pets', function(err, result) {
       done();
       if (err) {
@@ -24,6 +22,20 @@ app.get('/pets', function(req, res) {
 
 app.get('/pets/:petId', function(req, res) {
   res.send('get pet ' + req.params.petId);
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    if ( err ) {
+      throw err;
+    }
+    client.query('SELECT * FROM pets where id=$1::numeric', req.params.petId, function(err, result) {
+      done();
+      if (err) {
+        console.error(err);
+        res.send("Error " + err);
+      } else {
+        res.send( result.rows );
+      }
+    });
+  });
 });
 
 app.post('/pets/add', function(req, res) {
